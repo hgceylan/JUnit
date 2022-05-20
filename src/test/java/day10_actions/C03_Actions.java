7204b95c7d60101b58c1d53b261d498fe04c4dea
Hanife Ceylan <hanifeceylan064@gmail.com>
2022-05-20 23:41:54 +0300

    20/05/22

---
+++ src/test/java/day10_actions/C03_Actions.java
@@ -0,0 +1,18 @@
+package day10_actions;
+
+import org.junit.Test;
+
+public class C03_Actions {
+    @Test
+    public void test01(){
+        //1- Yeni bir class olusturalim: MouseActions1
+        //2- https://the-internet.herokuapp.com/context_menu sitesine gidelim
+        //3- Cizili alan uzerinde sag click yapalim
+        //4- Alert’te cikan yazinin “You selected a context menu” oldugunu
+        //test edelim.
+        //5- Tamam diyerek alert’i kapatalim
+        //6- Elemental Selenium linkine tiklayalim
+        //7- Acilan sayfada h1 taginda “Elemental Selenium” yazdigini test edelim
+
+    }
+}
