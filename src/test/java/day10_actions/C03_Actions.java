ceb62305ec1edc347d29e882f5ff20b7f3bc17f6
Hanife Ceylan <hanifeceylan064@gmail.com>
2022-05-21 08:35:31 +0300

    21/05/22

--- src/test/java/day10_actions/C03_Actions.java
+++ src/test/java/day10_actions/C03_Actions.java
@@ -1,18 +1,60 @@
 package day10_actions;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
 
-public class C03_Actions {
+import java.util.Set;
+
+public class C03_Actions extends TestBase {
     @Test
     public void test01(){
         //1- Yeni bir class olusturalim: MouseActions1
         //2- https://the-internet.herokuapp.com/context_menu sitesine gidelim
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+
         //3- Cizili alan uzerinde sag click yapalim
+        Actions actions= new Actions(driver);
+        WebElement cizgiliAlanElementi=driver.findElement(By.id("hot-spot"));
+        actions.contextClick(cizgiliAlanElementi).perform();
+
         //4- Alert’te cikan yazinin “You selected a context menu” oldugunu
         //test edelim.
+        String expectedYazi="You selected a context menu";
+        String actualYazi=driver.switchTo().alert().getText();
+        Assert.assertEquals(expectedYazi,actualYazi);
+
         //5- Tamam diyerek alert’i kapatalim
+        driver.switchTo().alert().accept();  // tamam demek icin
+
+
         //6- Elemental Selenium linkine tiklayalim
+        String ilkSayfaWindowHandleDegeri=driver.getWindowHandle();  // ilk sayfanin handle degeri
+        driver.findElement(By.xpath("//*[text()='Elemental Selenium']")).click(); // diger sayfaya gecis
+        Set<String> handleSeti=driver.getWindowHandles(); // diger sayfalarla birlikte tum sayfalarin handle degeri
+        String ikinciWHDegeri="";
+        System.out.println(handleSeti);
+
+        for (String each:handleSeti
+             ) {
+            if( !each.equals(ilkSayfaWindowHandleDegeri)){
+                ikinciWHDegeri=each;    // ikinci sayfanin windowHandle degerini elde etmis olacam
+            }
+
+        }
+
+
         //7- Acilan sayfada h1 taginda “Elemental Selenium” yazdigini test edelim
+        driver.switchTo().window(ikinciWHDegeri);
+        WebElement yaziElementi=driver.findElement(By.tagName("h1"));
+        String expectedYazi2="Elemental Selenium";
+        String actualYazi2=yaziElementi.getText();
+        Assert.assertEquals(expectedYazi2,actualYazi2);
+
 
     }
+
 }
